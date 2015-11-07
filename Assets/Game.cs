e2795065bf96dc5d040a7399dccc8dc6bbdd0da3
samowski <samuel.mierowski@gmail.com>
2015-11-07 14:50:17 +0100

    inital commit

---
+++ Assets/Game.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NotThatNumber
+{
+  class Game
+  {
+    public enum State
+    {
+      NeedName,
+      NeedNumber,
+      NeedContinueRound,
+      HaveRoundEnd,
+      HaveHighScore,
+      HaveThatNumber,
+    }
+      
+    // output
+    public HighScore Highscore { get; private set; }
+
+    public uint ThatNumber { get; private set; }
+
+    public uint Points { get; private set; }
+
+    // input
+    public bool ContinueRound = true;
+    public string Name = "";
+    public uint Number = 0u;
+
+    IEnumerator<State> gameIterator = null;
+    bool isThatNumber = false;
+    Random rng = new Random();
+
+    public Game(HighScore HighScore)
+    {
+      gameIterator = playGame();
+      this.Highscore = HighScore;
+    }
+
+    public State Advance()
+    {
+      gameIterator.MoveNext();
+      return gameIterator.Current;
+    }
+
+    IEnumerator<State> playGame()
+    {
+      while (true)
+      {
+        yield return State.HaveHighScore;
+
+        Points = 0u;
+
+        foreach (State state in playRound())
+        {
+          yield return state;
+        }
+
+        if (Points >= Highscore.Score && !isThatNumber)
+        {
+          yield return State.NeedName;
+          Highscore.Set(Points, Name);
+        }
+
+        yield return State.HaveRoundEnd;
+      }
+    }
+
+    IEnumerable<State> playRound()
+    {
+      while (true)
+      {
+        yield return State.NeedNumber;
+
+        ThatNumber = (uint)rng.Next(1, 5);
+        yield return State.HaveThatNumber;
+
+        isThatNumber = Number == ThatNumber;
+
+        if (isThatNumber)
+        {
+          break;
+        }
+        else
+        {
+          Points++;
+        }
+
+        yield return State.NeedContinueRound;
+
+        if (!ContinueRound)
+        {
+          break;
+        }
+      }
+    }
+  }
+}
---
+++ Assets/GameControl.cs
@@ -0,0 +1,236 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using NotThatNumber;
+
+
+public class GameControl : MonoBehaviour
+{
+	Game game = new Game(new MemoryHighScore());
+	Game.State state;
+
+	Button newRoundButton;
+
+	Text highscoreName;
+	Text highscoreScore;
+
+	Text score;
+
+	Button[] numberButtons = new Button[4];
+
+	Color disabledColor;
+
+	Button yesButton;
+	Button noButton;
+	Animator continueButtonAnimator;
+
+	InputField nameInput;
+
+	bool isAnimating = false;
+
+	void Start()
+	{
+		state = Game.State.HaveRoundEnd;//game.Advance();
+
+		newRoundButton = GameObject.Find("NewRoundButton").GetComponent<Button>();
+
+		highscoreName = GameObject.Find("Highscore/Name").GetComponent<Text>();
+		highscoreScore = GameObject.Find("Highscore/Score").GetComponent<Text>();
+		setHighscoreLabel();
+
+		score = GameObject.Find("Score/Score").GetComponent<Text>();
+
+		for (int i = 0; i < numberButtons.Length; i++)
+		{
+			GameObject button = GameObject.Find("Number" + (i + 1).ToString());
+			numberButtons[i] = button.GetComponent<Button>();
+			numberButtons[i].interactable = false;
+		}
+
+		disabledColor = numberButtons[0].colors.disabledColor;
+
+		yesButton = GameObject.Find("YesButton").GetComponent<Button>();
+		noButton = GameObject.Find("NoButton").GetComponent<Button>();
+		yesButton.interactable = false;
+		noButton.interactable = false;
+
+		continueButtonAnimator = GameObject.Find("ContinueButtons").GetComponent<Animator>();
+
+		nameInput = GameObject.Find("NameInput").GetComponent<InputField>();
+		nameInput.interactable = false;
+
+		//controlState();
+	}
+
+	void setHighscoreLabel()
+	{
+		string name = game.Highscore.Name;
+		highscoreName.text = string.IsNullOrEmpty(name) ? "---" : name;
+		highscoreScore.text = game.Highscore.Score.ToString();
+	}
+
+	public void selectNumber(int number)
+	{
+		Debug.Log(number.ToString() + " " + state);
+		//Debug.Log(state);
+		if (state == Game.State.NeedNumber)
+		{
+			game.Number = (uint)number;
+			foreach (var button in numberButtons)
+			{
+				button.interactable = false;
+			}
+			controlState();
+		}
+	}
+
+	public void selectContinue(bool selection)
+	{
+		Debug.Log(selection);
+		if (state == Game.State.NeedContinueRound)
+		{
+			game.ContinueRound = selection;
+
+			yesButton.interactable = false;
+			noButton.interactable = false;
+
+			continueButtonAnimator.SetBool("visible", false);
+
+			controlState();
+		}
+	}
+
+	public void selectName()
+	{
+		Debug.Log(nameInput.text);
+		if (state == Game.State.NeedName)
+		{
+			game.Name = nameInput.text;
+
+			nameInput.interactable = false;
+			nameInput.text = "";
+
+			controlState();
+		}
+	}
+
+	public void newRound()
+	{
+		if (state == Game.State.HaveRoundEnd)
+		{
+			newRoundButton.interactable = false;
+
+			controlState();
+		}
+	}
+
+	void controlState()
+	{
+		if (state == Game.State.NeedNumber || state == Game.State.NeedName || state == Game.State.NeedContinueRound || state == Game.State.HaveRoundEnd)
+		{
+			state = game.Advance();
+		} 
+
+		while (true)
+		{
+			Debug.Log(state.ToString());
+
+			if (state == Game.State.NeedNumber || state == Game.State.NeedName || state == Game.State.NeedContinueRound || state == Game.State.HaveRoundEnd)
+			{
+				if (state == Game.State.NeedNumber)
+				{
+					foreach (var button in numberButtons)
+					{
+						button.interactable = true;
+						setButtonColor(button, disabledColor);
+
+					}
+				}
+
+				if (state == Game.State.NeedContinueRound)
+				{
+					score.text = game.Points.ToString();
+
+					yesButton.interactable = true;
+					noButton.interactable = true;
+
+					continueButtonAnimator.SetBool("visible", true);
+				}
+
+				if (state == Game.State.NeedName)
+				{
+					nameInput.interactable = true;
+					nameInput.text = "";
+				}
+
+				if (state == Game.State.HaveRoundEnd)
+				{
+					score.text = "0";
+
+					newRoundButton.interactable = true;
+				}
+
+				break;
+			}
+			else
+			{
+				if (state == Game.State.HaveHighScore)
+				{
+					setHighscoreLabel(); // design flaw with highscore update
+				}
+				else if (state == Game.State.HaveThatNumber)
+				{
+					if (isAnimating)
+					{
+						isAnimating = false;
+					}
+					else
+					{
+						Debug.Log(game.ThatNumber);
+
+
+						isAnimating = true;
+
+						StartCoroutine(wait());
+
+						break;
+					}
+
+				}
+
+				state = game.Advance();
+			}
+
+		}
+	}
+
+	IEnumerator wait()
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			int rand = Random.Range(0, 3);
+			setButtonColor(numberButtons[rand], Color.red);
+			yield return new WaitForSeconds((i^2) * 0.1f);
+			setButtonColor(numberButtons[rand], disabledColor);	
+		}
+
+		yield return new WaitForSeconds((5^2) * 0.1f);
+
+		setButtonColor(numberButtons[game.ThatNumber - 1], game.Number == game.ThatNumber ? Color.red : Color.green);
+
+		controlState();
+	}
+
+	static void setButtonColor(Button button, Color color)
+	{
+		ColorBlock colors = button.colors;
+		colors.disabledColor = color;
+		button.colors = colors;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	
+	}
+}
