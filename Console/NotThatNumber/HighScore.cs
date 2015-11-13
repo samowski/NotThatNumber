71505feb03f337b77697b6e9b010e7167ec04fcb
samowski <samuel.mierowski@gmail.com>
2015-11-13 22:38:08 +0100

    add console version

---
+++ Console/NotThatNumber/HighScore.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+
+namespace NotThatNumber
+{
+  abstract class HighScore
+  {
+    public uint Score { get; protected set; }
+
+    public string Name { get; protected set; }
+
+    abstract public void Set(uint Score, string Name);
+  }
+
+  // Saves the highscore in Memory, lost after restart:
+
+  class MemoryHighScore : HighScore
+  {
+    public MemoryHighScore()
+    {      
+    }
+
+    public override void Set(uint Score, string Name)
+    {
+      this.Score = Score;
+      this.Name = Name;
+    }
+  }
+
+  // Uses a file to load and save the highscore:
+
+  class FileHighScore : HighScore
+  {
+    string path;
+
+    public FileHighScore(string Path)
+    {
+      path = Path;
+
+      if (File.Exists(path))
+      {
+        //read file:
+
+        var file = new StreamReader(path);
+        Score = Convert.ToUInt32(file.ReadLine());
+        Name = file.ReadLine();
+        file.Close();
+      }
+      else
+      {
+        File.WriteAllText(path, "0" + Environment.NewLine + Environment.NewLine);  // create file
+        Score = 0;
+        Name = "";
+      }
+    }
+
+    public override void Set(uint Score, string Name)
+    {
+      this.Score = Score;
+      this.Name = Name;
+
+      File.WriteAllText(path, Score.ToString() + Environment.NewLine + Name);
+    }
+  }
+}
+
---
+++ Console/NotThatNumber/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using NotThatNumber;
+
+namespace NotThatNumberConsoleVersion
+{
+  class Program
+  {
+    static string getHighScoreName()
+    {
+      string input;
+
+      Console.Write("Neuer Highscore! ");
+
+      do
+      {
+        Console.Write("Name: ");
+        input = Console.ReadLine();
+      }
+      while(input == "");
+
+      return input;
+    }
+
+    static uint getNumber()
+    {
+      bool validInput = false;
+      uint number;
+      string input;
+
+      do
+      {
+        Console.Write("Zahl von 1 bis 4 eingeben: ");
+        input = Console.ReadKey().KeyChar.ToString();//Console.ReadLine();
+        Console.WriteLine();
+
+        if (!UInt32.TryParse(input, out number))
+        {
+          continue;
+        }
+
+        validInput = number > 0 && number < 5;
+      }
+      while (!validInput);
+
+      return number;
+    }
+
+    static bool getContinue(string message)
+    {
+      Console.Write(message + "[J/N]");
+
+      while (true)
+      {
+        var key = Console.ReadKey(true).Key;
+
+        if (key == ConsoleKey.J)
+        {
+          Console.WriteLine();
+          return true;
+        }
+        else if (key == ConsoleKey.N)
+        {
+          Console.WriteLine();
+          return false;
+        }
+      }
+    }
+
+    static void Main()
+    {
+      Game game = new Game(new FileHighScore("highscore.txt"));
+
+      bool isGameRunning = true;
+
+      while (isGameRunning)
+      {
+        switch (game.Advance())
+        {
+          case Game.State.NeedName:
+            game.Name = getHighScoreName();
+            break;
+          case Game.State.NeedNumber:
+            game.Number = getNumber();
+            break;
+          case Game.State.NeedContinueRound:
+            game.ContinueRound = getContinue(String.Format("[Score {0}] weiter Spielen? ", game.Score));
+            break;
+          case Game.State.HaveRoundEnd:
+            isGameRunning = getContinue("Neue Runde? ");
+            break;
+          case Game.State.HaveRoundStart:
+            string name = game.Highscore.Name;
+            Console.WriteLine("Highscore [{0} : {1}]", string.IsNullOrEmpty(name) ? "Niemand" : name, game.Highscore.Score);
+            break;
+          case Game.State.HaveThatNumber:
+            Console.WriteLine("Die Zahl {0} wurde gezogen.{1}", game.ThatNumber, game.Number == game.ThatNumber ? " Runde verloren" : "");
+            break;
+          case Game.State.HaveHighScore:
+            break;
+          default:
+            Console.WriteLine("unkown error");
+            isGameRunning = false;
+            break;
+        }
+      }
+    }
+  }
+}
